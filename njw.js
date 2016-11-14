fe6090f5610ca073d894db39369864347f8cfe26
njw275 <njw275@nyu.edu>
2016-11-13 22:42:13 -0500

    Update njw.js

--- njw.js
+++ njw.js
@@ -1,7 +1,7 @@
 window.onload = slides;
 
 function slides() {
-  var images = ["GA.jpg","SYD.jpg","GC.jpg"];
+  var images = ["GA.jpg","SYD.jpg","GC.JPG"];
   var select = Math.floor(Math.random() * images.length);
   
   document.getElementById("topPhoto").src = images[select];
