71b52b0e03a2481a909b86e2509f657489448df6
njw275 <njw275@nyu.edu>
2016-11-14 16:54:33 -0500

    Update njw.js

--- njw.js
+++ njw.js
@@ -1,22 +1,24 @@
-window.onload = slides;
+$count = 0;
+imagesa = ["Thailand.jpg","Kangaroo.JPG","Hussein.JPG","HK.JPG","Bball.PNG"];
 
-var count = 0;
-var imagesa = ["Thailand.jpg","Kangaroo.JPG","Hussein.JPG","HK.JPG","Bball.PNG"];
+myimages = ["GA.jpg","SYD.jpg","GC.JPG"];
+$select = Math.floor(Math.random() * myimages.length);
 
-var myimages = ["GA.jpg","SYD.jpg","GC.JPG"];
-var select = Math.floor(Math.random() * myimages.length);
+$("#button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#aboutMe").offset().top-45
+    }, 2000);
+});
 
-function slides() {
-  document.getElementById("topPhoto").src = myimages[select];
+$(document).ready(slides() {
+  $('#topPhoto').src = myimages[$select];
   
-  if (count==imagesa.length){
-    count = 0;
+  if ($count==$images.length){
+    $count = 0;
   }
-  document.images[1].src = imagesa[count];
-  count++;
+  $(document).images[1].src = imagesa[$count];
+  $count++;
   setTimeout(slides, 5000);
-}
 
 
-
-window.addEventListener("hashchange", function() { scrollBy(0, -45) })
+});
